66cb3ca9c4dca3f922a14af0afcd1bb5a809bcba
Hanapibagas <hanafibagas4@gmail.com>
2023-07-24 16:26:38 +0800

    .

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,22 +1,27 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+import os
 import speech_recognition as sr
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
 
 app = Flask(__name__)
 
-def speech_to_text():
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def convert_to_wav(mp4_path, wav_path):
+    # Menggunakan moviepy untuk mengkonversi file MP4 menjadi WAV
+    ffmpeg_extract_audio(mp4_path, wav_path)
+
+def speech_to_text(file_path):
     recognizer = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        print("Silakan mulai berbicara...")
+    with sr.AudioFile(file_path) as source:
+        print("Mengubah suara menjadi teks...")
         recognizer.adjust_for_ambient_noise(source)
-
-        audio = recognizer.listen(source)
+        audio = recognizer.record(source)
 
     try:
-        print("Mengubah suara menjadi teks...")
         text = recognizer.recognize_google(audio, language="id-ID")
-
         print("Teks hasil pengenalan suara:")
         print(text)
         return text
@@ -29,18 +34,51 @@ def speech_to_text():
 
 @app.route("/api/speech-to-text", methods=["POST"])
 def api_speech_to_text():
-    text = speech_to_text()
-    if text:
-        response = {
-            "status": "success",
-            "text": text
-        }
-    else:
-        response = {
+    if 'audio' not in request.files:
+        return jsonify({
             "status": "error",
-            "message": "Pengenalan suara gagal. Coba lagi."
-        }
-    return jsonify(response)
+            "message": "Tidak ada file audio yang dikirim."
+        })
+
+    audio_file = request.files['audio']
+    if audio_file.filename == '':
+        return jsonify({
+            "status": "error",
+            "message": "File audio tidak valid."
+        })
+
+    if audio_file:
+        # Simpan file audio MP4 secara sementara
+        mp4_path = os.path.join(app.config['UPLOAD_FOLDER'], audio_file.filename)
+        audio_file.save(mp4_path)
+
+        # Konversi file MP4 menjadi WAV
+        wav_path = os.path.splitext(mp4_path)[0] + ".wav"
+        convert_to_wav(mp4_path, wav_path)
+
+        # Panggil fungsi speech_to_text dengan file audio WAV sebagai parameter
+        text = speech_to_text(wav_path)
+
+        # Hapus file audio yang telah disimpan secara sementara
+        os.remove(mp4_path)
+        os.remove(wav_path)
+
+        if text:
+            response = {
+                "status": "success",
+                "text": text
+            }
+        else:
+            response = {
+                "status": "error",
+                "message": "Pengenalan suara gagal. Coba lagi."
+            }
+        return jsonify(response)
+    else:
+        return jsonify({
+            "status": "error",
+            "message": "Tidak dapat mengakses file audio."
+        })
 
 if __name__ == "__main__":
     app.run(debug=True)
